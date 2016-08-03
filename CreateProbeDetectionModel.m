53a22c449df46867aaa572437c91d7a9179bfbc2
Bernard Llanos <llanos@ualberta.ca>
2016-08-03 11:34:45 -0600

    Refactoring, round 2:
    - Extracted a function for classifying interest points using PCA as opposed to linear regression.

--- CreateProbeDetectionModel.m
+++ CreateProbeDetectionModel.m
@@ -27,7 +27,8 @@
 % - lengths: Distances of edges of bands from the active end of the probe,
 %     including a distance of 0.0 for the active end of the probe, and,
 %     optionally, a distance for the other end of the probe (i.e. the
-%     length of the entire probe).
+%     length of the entire probe). Distances are measured along the probe's
+%     axis of cylindrical symmetry.
 % - widths: Width of the probe at the edges of bands. Widths must include
 %     the ends of the probe, except for ends that taper to points. The
 %     elements of 'widths' should correspond to elements of 'lengths'
@@ -75,10 +76,14 @@ I_annotations_filename = 'C:\Users\Bernard\Documents\Data\20160725_probes_lotus\
 % Annotation extraction parameters
 annotation_corner_search_width = 4; % Set to zero to use centers of user-marked annotations as opposed to nearby corner features
 
+% Parameters for interpreting annotated points
+point_alignment_outlier_threshold = 3;
+
 % Debugging tools
 display_original_image = false;
 display_annotations_image = false;
 display_extracted_annotations = false;
+display_model_from_image = true;
 
 %% Load images and obtain adjusted centers of user-marked annotations
 
@@ -110,7 +115,7 @@ if display_extracted_annotations
     hold on
     plot(interest_points(:, 1), interest_points(:, 2), 'ro')
     if annotation_corner_search_width
-        title('Extracted user-marked interest points (red o) and other corner features');
+        title('Final marked interest points (red o) and other corner features');
         for rect = feature_search_windows'
             rectangle('Position', rect, 'EdgeColor', 'b');
         end
@@ -120,208 +125,61 @@ if display_extracted_annotations
             end
         end
     else
-        title('Extracted user-marked interest points (red o)');
+        title('Extracted interest points (red o)');
     end
+    hold off
+end
+
+%% Model the interest points as a series of bands on a linear probe object
+
+[...
+    model_from_image, model_from_image_lengths, model_from_image_axes, model_to_image_transform...
+] = bilateralModel(...
+    interest_points, point_alignment_outlier_threshold, true...
+);
+
+% The probe tips are marked with single points. All other probe segments
+% must be marked with a point along each side of the probe.
+%
+% Since we are performing calibration, failure to satisfy this condition is
+% an error. In a detection scenario, we would just eliminate the outlier
+% points and continue.
+if ~isempty(model_from_image.unmatched)
+    error('Not all probe color band junctions are marked with two points - One on each edge of the probe.');
+end
+
+if display_model_from_image
+    figure;
+    imshow(I);
+    title('Classified interest points (blue, black = tips; green, red = above/below first PCA component; yellow = unmatched)');
+    hold on
+    
+    % Plot points
+    if isfield(model_from_image, 'head')
+        head = [model_from_image.head, 1] * model_to_image_transform;
+        scatter(head(1), head(2), 'b')
+    end
+    if isfield(model_from_image, 'tail')
+        tail = [model_from_image.tail, 1] * model_to_image_transform;
+        scatter(tail(1), tail(2), 'k')
+    end
+    above = [model_from_image.above, ones(size(model_from_image.above, 1), 1)] * model_to_image_transform;
+    scatter(above(:, 1), above(:, 2), 'g')
+    below = [model_from_image.below, ones(size(model_from_image.below, 1), 1)] * model_to_image_transform;
+    scatter(below(:, 1), below(:, 2), 'r')
+    unmatched = [model_from_image.unmatched, ones(size(model_from_image.unmatched, 1), 1)] * model_to_image_transform;
+    scatter(unmatched(:, 1), unmatched(:, 2), 'y')
+    
+    % Plot PCA lines
+    line_points = lineToBorderPoints(model_from_image_axes, [image_height, image_width]);
+    line(line_points(1, [1,3])', line_points(1, [2,4])', 'Color', 'c');
+    line(line_points(2, [1,3])', line_points(2, [2,4])', 'Color', 'm');
     hold off
 end
 
 
 
-
-%% Model the interest points as a series of segments
-
-% Determine probe centerline with linear regression
-X = [ones(n_annotations, 1), feature_points(:, 1)];
-intercept_slope = X \ feature_points(:, 2);
-hold on
-y_line = X * intercept_slope;
-plot(feature_points(:, 1),y_line,'g--')
-hold off
-
-% Express interest points in a coordinate space where the centerline is the
-% x-axis, and the leftmost interest point's closest projection onto the
-% line is on the y-axis
-% See https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-a = intercept_slope(2);
-b = -1;
-c = intercept_slope(1);
-feature_points_on_line_x = (b * (b * feature_points(:, 1) - a * feature_points(:, 2)) - a * c)...
-    / (a^2 + b^2);
-feature_points_on_line_y = (a * (-b * feature_points(:, 1) + a * feature_points(:, 2)) - b * c)...
-    / (a^2 + b^2);
-% distances_to_line = sqrt((feature_points(:, 1) - feature_points_on_line_x) .^ 2 ...
-%     + (feature_points(:, 2) - feature_points_on_line_y) .^ 2);
-[x_min_line, min_ind] = min(feature_points_on_line_x);
-line_origin = [x_min_line, feature_points_on_line_y(min_ind)];
-line_direction = [1, intercept_slope(2)];
-line_direction = line_direction / norm(line_direction);
-line_direction_rep = repmat(line_direction, n_annotations, 1);
-feature_points_to_line_origin = feature_points - repmat(line_origin, n_annotations, 1);
-feature_points_line_coordinates_x = dot(feature_points_to_line_origin, line_direction_rep, 2);
-feature_points_line_coordinates_y = feature_points_to_line_origin - repmat(feature_points_line_coordinates_x, 1, 2) .* line_direction_rep;
-feature_points_line_coordinates_y = sign(feature_points_line_coordinates_y(:, 2))...
-    .* sqrt(dot(feature_points_line_coordinates_y, feature_points_line_coordinates_y, 2));
-
-% Partition points into those above and below the line
-above_line_filter = feature_points_line_coordinates_y > 0;
-feature_points_above = feature_points(above_line_filter, :);
-feature_points_below = feature_points(~above_line_filter, :);
-feature_points_line_coordinates_above = [
-        feature_points_line_coordinates_x(above_line_filter),...
-        feature_points_line_coordinates_y(above_line_filter)
-    ];
-feature_points_line_coordinates_below = [
-        feature_points_line_coordinates_x(~above_line_filter),...
-        feature_points_line_coordinates_y(~above_line_filter)
-    ];
-
-% Sort points by x-coordinate
-A = [feature_points_line_coordinates_above, feature_points_above];
-A = sortrows(A);
-feature_points_line_coordinates_above = A(:, 1:2);
-feature_points_above = A(:, 3:4);
-
-A = [feature_points_line_coordinates_below, feature_points_below];
-A = sortrows(A);
-feature_points_line_coordinates_below = A(:, 1:2);
-feature_points_below = A(:, 3:4);
-
-figure
-hold on
-plot(feature_points_line_coordinates_x, zeros(n_annotations, 1),'k--')
-plot(feature_points_line_coordinates_above(:, 1), feature_points_line_coordinates_above(:, 2), 'g.')
-plot(feature_points_line_coordinates_below(:, 1), feature_points_line_coordinates_below(:, 2), 'r.')
-title('Probe interest points in centerline coordinate space')
-xlabel('X')
-ylabel('Y')
-
-% Collapse the points into a single row by matching points above and below
-% the line
-n_feature_points_above = size(feature_points_line_coordinates_above, 1);
-min_x_separations_above = zeros(n_feature_points_above, 1);
-min_x_separation_indices_above = zeros(n_feature_points_above, 1);
-for i = 1:n_feature_points_above
-    [min_x_separations_above(i), min_x_separation_indices_above(i)] =...
-        min(abs(feature_points_line_coordinates_below(:, 1) - feature_points_line_coordinates_above(i, 1)));
-end
-
-n_feature_points_below = size(feature_points_line_coordinates_below, 1);
-min_x_separations_below = zeros(n_feature_points_below, 1);
-min_x_separation_indices_below = zeros(n_feature_points_below, 1);
-for i = 1:n_feature_points_below
-    [min_x_separations_below(i), min_x_separation_indices_below(i)] =...
-        min(abs(feature_points_line_coordinates_above(:, 1) - feature_points_line_coordinates_below(i, 1)));
-end
-
-% Identify mismatched points based on mutual selection
-inliers_above = (min_x_separation_indices_below(min_x_separation_indices_above) == (1:n_feature_points_above).');
-inliers_below = (min_x_separation_indices_above(min_x_separation_indices_below) == (1:n_feature_points_below).');
-
-% Identify mismatched points with a statistical analysis
-% Reference: MATLAB documentation page on "Inconsistent Data"
-
-% Outlier separation in x-coordinates
-inlier_separations = [min_x_separations_above(inliers_above); min_x_separations_below(inliers_below)];
-mu = mean(inlier_separations);
-sigma = std(inlier_separations);
-
-mu_rep = repmat(mu,n_feature_points_above,1);
-sigma_rep = repmat(sigma,n_feature_points_above,1);
-point_alignment_outlier_threshold = 3;
-fprintf('(Parameter) Outlier threshold (number of standard deviations) for detecting interest points on different bands: %g\n', point_alignment_outlier_threshold);
-inliers_above = inliers_above & (abs(min_x_separations_above - mu_rep) < point_alignment_outlier_threshold * sigma_rep);
-
-mu_rep = repmat(mu,n_feature_points_below,1);
-sigma_rep = repmat(sigma,n_feature_points_below,1);
-inliers_below = inliers_below & (abs(min_x_separations_below - mu_rep) < point_alignment_outlier_threshold * sigma_rep);
-
-% Outlier separation in y-coordinates
-y_separations_above = abs(feature_points_line_coordinates_above(:, 2)...
-    + feature_points_line_coordinates_below(min_x_separation_indices_above, 2));
-y_separations_below = abs(feature_points_line_coordinates_below(:, 2)...
-    + feature_points_line_coordinates_above(min_x_separation_indices_below, 2));
-inlier_separations = abs([
-    feature_points_line_coordinates_above(inliers_above, 2)...
-    + feature_points_line_coordinates_below(min_x_separation_indices_above(inliers_above), 2);
-    feature_points_line_coordinates_below(inliers_below, 2)...
-    + feature_points_line_coordinates_above(min_x_separation_indices_below(inliers_below), 2);]);
-mu = mean(inlier_separations);
-sigma = std(inlier_separations);
-
-mu_rep = repmat(mu,n_feature_points_above,1);
-sigma_rep = repmat(sigma,n_feature_points_above,1);
-inliers_above = inliers_above & (abs(y_separations_above - mu_rep) < point_alignment_outlier_threshold * sigma_rep);
-
-mu_rep = repmat(mu,n_feature_points_below,1);
-sigma_rep = repmat(sigma,n_feature_points_below,1);
-inliers_below = inliers_below & (abs(y_separations_below - mu_rep) < point_alignment_outlier_threshold * sigma_rep);
-
-% It is acceptable if the probe tips are marked with single points.
-% All other probe segments must be marked with a point on each side of the
-% centerline.
-%
-% Since we are performing calibration, failure to satisfy this condition is
-% an error. In a detection scenario, we would just eliminate the outlier
-% points and continue.
-A = [
-    feature_points_line_coordinates_above(:, 1), double(inliers_above);
-    feature_points_line_coordinates_below(:, 1), double(inliers_below)
-    ];
-A = sortrows(A);
-all_inliers = A(:, end);
-n_outliers = sum(~all_inliers);
-if (~all_inliers(1) + ~all_inliers(end)) ~= n_outliers
-    error('Not all probe segment junctions are marked with two points, above and below the centerline.');
-end
-probe_has_left_tip = ~all_inliers(1);
-probe_has_right_tip = ~all_inliers(end);
-
-% Collect the single row of points
-n_boundary_points = n_feature_points_above + sum(~inliers_below);
-boundary_points = zeros(n_boundary_points, 2);
-segments_to_points_above = zeros(n_boundary_points, 1);
-segments_to_points_below = zeros(n_boundary_points, 1);
-start_index = 1;
-index_above = 1;
-end_index = n_boundary_points;
-if probe_has_left_tip
-    boundary_points(1, :) = 0;
-    if ~inliers_above(1)
-        segments_to_points_above(1) = 1;
-        index_above = 2;
-    else
-        segments_to_points_below(1) = 1;
-    end
-    start_index = 2;
-end
-if probe_has_right_tip
-    boundary_points(end, 2) = 0;
-    if ~inliers_above(end)
-        segments_to_points_above(end) = n_feature_points_above;
-        boundary_points(end, 1) = feature_points_line_coordinates_above(end, 1);
-    else
-        segments_to_points_below(end) = n_feature_points_below;
-        boundary_points(end, 1) = feature_points_line_coordinates_below(end, 1);
-    end
-    end_index = n_boundary_points - 1;
-end
-
-for i = start_index:end_index
-    match_index = min_x_separation_indices_above(index_above);
-    boundary_points(i, 1) = mean([
-        feature_points_line_coordinates_above(index_above, 1), feature_points_line_coordinates_below(match_index, 1)
-        ]);
-    boundary_points(i, 2) = feature_points_line_coordinates_above(index_above, 2) - feature_points_line_coordinates_below(match_index, 2);
-    index_above = index_above + 1;
-end
-
-% Plot for validation
-plot(boundary_points(:, 1), boundary_points(:, 2), 'b.')
-legend('Regression centerline', 'Interest points above line',...
-    'Interest points below line', 'Merged interest points with double width');
-hold off
-
-%% Match segment model to the user-supplied model of the probe
+%% Match model extracted from the image to the user-supplied measurements of the probe
 load('C:\Users\Bernard\Documents\Data\20160725_probes_lotus\redPen.mat');
 if ~exist('probe', 'var')
     error('No variable called ''probe'' is loaded.')
---
+++ bilateralModel.m
@@ -0,0 +1,255 @@
+function [ model, lengths, axes, transform ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+% BILATERALMODEL  Interpret points as arranged symmetrically around a line segment
+%
+% ## Syntax
+% model = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+% [ model, lengths ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+% [ model, lengths, axes ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+%
+% ## Description
+% model = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+%   Returns the input points in the coordinates of their PCA components,
+%   organized into pairs where possible.
+%
+% [ model, lengths ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+%   Additionally returns a summarization of data in `model` with respect to
+%   the first PCA component of the input points.
+%
+% [ model, lengths, axes ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+%   Additionally returns the PCA components of the input points in the form of
+%   lines in the original coordinate frame of the points.
+%
+% [ model, lengths, axes, transform ] = bilateralModel( points, point_alignment_outlier_threshold, distinguish_tips )
+%   Additionally returns the transformation matrix that converts the points
+%   from the PCA components space to the reference frame of the original
+%   data.
+%
+% ## Input Arguments
+%
+% points -- 2D point coordinates
+%   An n x 2 array, where 'n' is the number of points, and where the
+%   columns store point x and y coordinates, respectively.
+%
+% point_alignment_outlier_threshold -- Outlier threshold in standard deviations
+%   The number of standard deviations from the mean separation of the first
+%   coordinates of points paired based on their first coordinates in PCA
+%   component space at which a pair is considered to be a false match.
+%
+% distinguish_tips -- Endpoint identification flag
+%   If `distinguish_tips` is true, points at extremal positions along the
+%   line corresponding to the first PCA component that have no matches
+%   across the line will be returned separately as the tips of the dataset.
+%
+% ## Output Arguments
+%
+% model -- Bilaterally-symmetric representation of points
+%   A structure with the following fields:
+%   - above: Points located above the first axis in the PCA component
+%       space, that are paired with points below the first axis based on
+%       the similarity of their first coordinates. `above(i)` is paired
+%       with `below(i)`.
+%   - below: Points located below the first axis in the PCA component
+%       space, that are paired with points above the first axis based on
+%       the similarity of their first coordinates. `above(i)` is paired
+%       with `below(i)`.
+%   - unmatched: Points located above or below the first axis in the PCA
+%       component space for which no matches were found based on similar
+%       first coordinate values. In detail, `unmatched` is the union of
+%       the following:
+%       - Points whose closest neighbours (by first coordinate in PCA component
+%         space) did not select them as their closest neighbours in return.
+%       - Points whose closest neighbours (by first coordinate in PCA component
+%         space) are at a distance of more than
+%         `point_alignment_outlier_threshold` times the standard deviation
+%         of closest neighbour separations from the mean closest neighbour
+%         separation.
+%       `unmatched` may be an empty array.
+%
+%   All fields store k x 2 arrays, where k varies, but is equal to the
+%   number of points meeting the criteria corresponding to the field, and
+%   the columns store the x and y coordinates of points in the coordinate
+%   space defined by the PCA components of `points`. `above` has the same
+%   dimensions as `below`, because these points are paired. All arrays are
+%   sorted by their first column.
+%
+%   The `distinguish_tips` flag allows for special treatment of extremal
+%   points, and may result in additional fields:
+%   - If `distinguish_tips` is true, and the first point in `unmatched` has the
+%     lowest first coordinate value of all points, then that point is
+%     removed from `unmatched` and placed in a field called `head`.
+%   - Next, if `distinguish_tips` is true, and the last point in `unmatched` has
+%     the highest first coordinate value of all points, then that point is
+%     removed from `unmatched` and placed in a field called `tail`.
+%
+% lengths -- Unification of matched points along the first PCA component line
+%   A column vector storing the averaged first PCA component coordinates of the
+%   paired points in `model.above` and `model.below`, as well as extremal
+%   points, expressed relative to the leftmost point(s).
+%
+%   Specifically, let 's' equal `model.head(1)`, if `model.head` exists, or
+%   otherwise, the average of `model.above(1, 1)` and `model.below(1, 1)`.
+%
+%   The first element of `lengths` has a value of zero and corresponds to
+%   `model.head`, if `model.head` exists, or otherwise, to the pair of
+%   `model.above(0, :)` and `model.below(0, :)`. The last element of
+%   `lengths` has a value of `model.tail(1) - s` and corresponds to
+%   `model.tail`, if `model.tail` exists. Otherwise, the last element of
+%   `lengths` corresponds to the pair `model.above(end, :)` and
+%   `model.below(end, :)` and is equal to
+%   `mean([model.above(end, 1), model.below(end, 1)]) - s`.
+%
+%   The k-th intermediate element of `lengths` corresponds to the pair
+%   `model.above(k, :)` and `model.below(k, :)` and is equal to
+%   `mean([model.above(k, 1), model.below(k, 1)]) - s`.
+%
+% axes -- Lines representing PCA components
+%   An 2 x 3 array, where `axes(i, :)` is the parameters of a line
+%   representing the i-th PCA component of `points` in the same frame of
+%   reference as `points`. `axes(i, :)` is the line described by the
+%   equation `axes(i, 1) * x + axes(i, 2) * y + axes(i, 3) = 0`.
+%
+%   For convenience, lines have been normalized as described here:
+%   http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/BEARDSLEY/node2.html
+%
+% transform -- Map back from PCA component space
+%   A 3 x 2 array, of the form `[coeff mu].'`, where `coeff` is the
+%   principal components expressed in the reference frame of the original
+%   data, and `mu` is the vector of variable means. `transform` can be used
+%   to map points in `model` back into their original frame of reference
+%   using the expression `[model_points, 1] * transform` (where '1' is a
+%   column vector of ones).
+%
+% ## Notes
+% - "Bilateral symmetry" refers to the fact that the function attempts to find
+%   pairs of points that are approximately at the same position along the
+%   first PCA component line, but which are on opposite sides of the first PCA
+%   component line. There is no symmetry in the sense that pairs of points
+%   may be at unequal distances from the first PCA component line.
+% - Suppose the first PCA component line happens to be centered between pairs of
+%   points on either side, and the points represent edge points on an image
+%   of a linear object. This does not mean that the PCA component line
+%   corresponds to the midline of the object, due to projective distortion.
+%
+% See also pca
+
+% Bernard Llanos
+% Spring 2016 research assistantship supervised by Dr. Y.H. Yang
+% University of Alberta, Department of Computing Science
+% File created August 2, 2016
+
+nargoutchk(1, 4);
+narginchk(3, 3);
+
+% Express the points in the coordinate space of their principal components
+% using Principal Components Analysis
+if nargout > 2
+    [coeff,score,~, ~, ~, mu] = pca(points);
+    
+    % Express the PCA component vectors as lines in the space of the original data
+    dx = coeff(1, :).';
+    dy = coeff(2, :).';
+    c = ones(2, 1);
+    a = c ./ ((dx * mu(2) ./ dy) - mu(1));
+    b = -a .* dx ./ dy;
+    % Normalize
+    scale = sqrt(a .^ 2 + b .^ 2);
+    axes = [a, b, c] ./ repmat(scale, 1, 3);
+    
+    transform = [coeff, mu.'].';
+else
+    [~,score] = pca(points);
+end
+
+% Partition points into those above and below the first component
+above_line_filter = score(:, 2) > 0;
+points_above = score(above_line_filter, :);
+points_below = score(~above_line_filter, :);
+
+% Sort points by first coordinate
+points_above = sortrows(points_above);
+points_below = sortrows(points_below);
+
+% Organize the points into a single row by matching points above and below
+% the first component. Unmatched points will be kept separate.
+n_points_above = size(points_above, 1);
+min_separations_above = zeros(n_points_above, 1);
+min_x_separation_indices_above = zeros(n_points_above, 1);
+for i = 1:n_points_above
+    [min_separations_above(i), min_x_separation_indices_above(i)] =...
+        min(abs(points_below(:, 1) - points_above(i, 1)));
+end
+
+n_points_below = size(points_below, 1);
+min_separations_below = zeros(n_points_below, 1);
+min_x_separation_indices_below = zeros(n_points_below, 1);
+for i = 1:n_points_below
+    [min_separations_below(i), min_x_separation_indices_below(i)] =...
+        min(abs(points_above(:, 1) - points_below(i, 1)));
+end
+
+% Identify mismatched points based on mutual selection
+inliers_above_filter = (min_x_separation_indices_below(min_x_separation_indices_above) == (1:n_points_above).');
+inliers_below_filter = (min_x_separation_indices_above(min_x_separation_indices_below) == (1:n_points_below).');
+
+% Identify mismatched points with a statistical analysis
+% Reference: MATLAB documentation page on "Inconsistent Data"
+% Outlier separation in x-coordinates
+inlier_separations = [min_separations_above(inliers_above_filter); min_separations_below(inliers_below_filter)];
+mu_separations = mean(inlier_separations);
+sigma_separations = std(inlier_separations);
+
+mu_separations_rep = repmat(mu_separations,n_points_above,1);
+sigma_separations_rep = repmat(sigma_separations,n_points_above,1);
+inliers_above_filter = inliers_above_filter & (abs(min_separations_above - mu_separations_rep) < point_alignment_outlier_threshold * sigma_separations_rep);
+
+mu_separations_rep = repmat(mu_separations,n_points_below,1);
+sigma_separations_rep = repmat(sigma_separations,n_points_below,1);
+inliers_below_filter = inliers_below_filter & (abs(min_separations_below - mu_separations_rep) < point_alignment_outlier_threshold * sigma_separations_rep);
+
+% Organize mismatched points according to their positions along the first
+% component axis
+unmatched = [
+        points_above(~inliers_above_filter, :);
+        points_below(~inliers_below_filter, :)
+    ];
+unmatched = sortrows(unmatched);
+
+% Output the points
+model = struct(...
+        'above', {points_above(inliers_above_filter, :)},...
+        'below', {points_below(inliers_below_filter, :)}...
+    );
+if distinguish_tips && ~isempty(unmatched)
+    if unmatched(1, 1) < model.above(1, 1) && unmatched(1, 1) < model.below(1, 1)
+        model.head = unmatched(1, :);
+        unmatched = unmatched(2:end, :);
+    end
+    if ~isempty(unmatched) && unmatched(end, 1) > model.above(end, 1) && unmatched(end, 1) > model.below(end, 1)
+        model.tail = unmatched(end, :);
+        unmatched = unmatched(1:(end - 1), :);
+    end
+end
+model.unmatched = unmatched;
+
+if nargout > 1
+    n_intermediate_lengths = size(model.above, 1);
+    start_offset = isfield(model, 'head');
+    end_offset = isfield(model, 'tail');
+    lengths = zeros(n_intermediate_lengths + start_offset + end_offset, 1);
+    if start_offset
+        s = model.head(1);
+    else
+        s = mean([model.above(1, 1), model.below(1, 1)]);
+    end
+    for i = (start_offset + 1):(n_intermediate_lengths + start_offset)
+        lengths(i) = mean([
+                model.above(i - start_offset, 1),...
+                model.below(i - start_offset, 1)
+            ]) - s;
+    end
+    if end_offset
+        lengths(end) = model.tail(1) - s;
+    end
+end
+
+end
