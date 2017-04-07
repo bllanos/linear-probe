a3bf488955785c0a9218b638d7d9715c5c6bac87
Bernard Llanos <llanos@ualberta.ca>
2017-04-06 20:07:09 -0600

    Efficiency improvement. Never use cellfun() or arrayfun().

--- isOnBothSidesOfLine.m
+++ isOnBothSidesOfLine.m
@@ -47,18 +47,22 @@ nargoutchk(1, 1);
 narginchk(2, 2);
 
 n = size(p, 1);
-l_rep = repmat({l}, n, 1);
 
+p_filter = false(n, 1);
 if all(size(l) == [1 3])
     % Line defined by a homogenous equation
-    [~, signs] = cellfun(@distanceToHomog2DLine, p, l_rep, 'UniformOutput', false);
+    for i = 1:n
+        [~, signs] = distanceToHomog2DLine(p{i}, l);
+        p_filter(i) = isOnBothSides(signs);
+    end
 elseif all(size(l) == [2 2])
     % Line defined by two endpoints
-    [~, signs] = cellfun(@distanceToLine, p, l_rep, 'UniformOutput', false);
+    for i = 1:n
+        [~, signs] = distanceToLine(p{i}, l);
+        p_filter(i) = isOnBothSides(signs);
+    end
 else
     error('Unknown form of the second input argument `l` or `line_points`.')
 end
-
-p_filter = cellfun(@isOnBothSides, signs, 'UniformOutput', true);
 end
 
